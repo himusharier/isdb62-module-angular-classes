4225157807e351971171db75ac7f92c3797b88dd
Sharier Himu <himusharier@gmail.com>
2025-02-20 02:04:25 +0600

    angular practice project 01 added

---
+++ angular-practice-01/src/app/todos/todos.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { TodosService } from '../services/todos.service';
+import { Todo } from '../model/todo.type';
+import { catchError } from 'rxjs';
+import { NgIf } from '@angular/common';
+import { TodoItemComponent } from '../components/todo-item/todo-item.component';
+
+@Component({
+  selector: 'app-todos',
+  imports: [TodoItemComponent], //NgIf
+  templateUrl: './todos.component.html',
+  styleUrl: './todos.component.scss'
+})
+export class TodosComponent implements OnInit {
+  todoService = inject(TodosService);
+  todoItems = signal<Array<Todo>>([]);
+
+  ngOnInit(): void {
+    /*console.log(this.todoService.todoItems);
+    this.todoItems.set(this.todoService.todoItems);*/
+
+    this.todoService.getTodosFromApi().pipe(
+      catchError(error => {
+        console.log(error);
+        throw(error);
+      })
+    ). subscribe(todos => {
+      this.todoItems.set(todos);
+    });
+  }
+
+}
